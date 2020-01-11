93ae602a2f55182f2c8f11a21c73d8e2dd5439e6
HyunjoonCho <jhj09016@gmail.com>
2020-01-11 13:50:36 +0900

    Mission Failed

--- app/src/main/java/com/example/blink/MainActivity.kt
+++ app/src/main/java/com/example/blink/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.blink
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.airbnb.lottie.LottieAnimationView
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        /*
+        val animationView = findViewById(R.id.blink_view) as LottieAnimationView
+        animationView.setAnimation("blink_faster.json")
+        animationView.scale = 4.0f
+        animationView.loop(true)
+        animationView.playAnimation()
+        */
     }
 }
